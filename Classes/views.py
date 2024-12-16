23ec69af5f18cf550716b72735a1a828b24e1a2c
Rhys Tyler <rhystyler.cardiff@gmail.com>
2024-12-16 17:27:36 +0000

    add templates and basic styling

--- Classes/views.py
+++ Classes/views.py
@@ -5,5 +5,6 @@ from .models import GymClass
 # Create your views here.
 class ClassesList(generic.ListView):
     queryset = GymClass.objects.all()
-    template_name = "Classes/classes_list.html"
+    template_name = "Classes/index.html"
     context_object_name = 'gym_classes'
+    paginate_by = 6
