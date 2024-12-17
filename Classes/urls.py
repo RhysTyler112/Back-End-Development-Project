0bff73035cbff4bebf9d9933d1819d54777dad40
Rhys Tyler <rhystyler.cardiff@gmail.com>
2024-12-17 17:30:24 +0000

    add view, urls and html

--- Classes/urls.py
+++ Classes/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.ClassesList.as_view(), name='home'),
+     path('New Booking/', views.create_booking, name='booking_create'),
 ]
--- Classes/views.py
+++ Classes/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views import generic
-from .models import GymClass
+from .models import GymClass, Booking
 
 # Create your views here.
 class ClassesList(generic.ListView):
@@ -8,3 +8,17 @@ class ClassesList(generic.ListView):
     template_name = "Classes/index.html"
     context_object_name = 'gym_classes'
     paginate_by = 6
+
+#@login_required
+def create_booking(request):
+    if request.method == 'POST':
+        form = BookingForm(request.POST)
+        if form.is_valid():
+            booking = form.save(commit=False)
+            booking.user = request.user  # Assign the logged-in user to the booking
+            booking.save()
+            return redirect('booking_list')  # Redirect to booking list or desired success URL
+    else:
+        form = BookingForm()
+
+    return render(request, 'Classes/booking_form.html', {'form': form})
