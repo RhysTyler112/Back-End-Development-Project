ae55e221ea7527787092eb01afe351d26d7c465f
Rhys Tyler <rhystyler.cardiff@gmail.com>
2024-12-21 15:50:08 +0000

    fix code in admin.py

--- Classes/admin.py
+++ Classes/admin.py
@@ -1,19 +1,20 @@
 from django.contrib import admin
 from django.utils.html import format_html
+from django.urls import reverse
 from .models import GymClass, Booking, Membership
 
 class GymClassAdmin(admin.ModelAdmin):
     list_display = ('name', 'date', 'time', 'edit_button', 'delete_button')
 
     def edit_button(self, obj):
-        # Generates an Edit link for the object
-        return format_html('<a class="btn btn-primary btn-sm" href="{}">Edit</a>', 
-                           f'/admin/app_name/gymclass/{obj.id}/change/')
+        # Generate the URL for the edit page dynamically
+        edit_url = reverse('admin:Classes_gymclass_change', args=[obj.id])
+        return format_html('<a class="btn btn-primary btn-sm" href="{}">Edit</a>', edit_url)
 
     def delete_button(self, obj):
-        # Generates a Delete link for the object
-        return format_html('<a class="btn btn-danger btn-sm" href="{}">Delete</a>', 
-                           f'/admin/app_name/gymclass/{obj.id}/delete/')
+        # Generate the URL for the delete page dynamically
+        delete_url = reverse('admin:Classes_gymclass_delete', args=[obj.id])
+        return format_html('<a class="btn btn-danger btn-sm" href="{}">Delete</a>', delete_url)
 
     edit_button.short_description = 'Edit'
     delete_button.short_description = 'Delete'
@@ -22,14 +23,14 @@ class BookingAdmin(admin.ModelAdmin):
     list_display = ('gym_class', 'user', 'edit_button', 'delete_button')
 
     def edit_button(self, obj):
-        # Generates an Edit link for the object
-        return format_html('<a class="btn btn-primary btn-sm" href="{}">Edit</a>', 
-                           f'/admin/app_name/booking/{obj.id}/change/')
+        # Generate the URL for the edit page dynamically
+        edit_url = reverse('admin:Classes_booking_change', args=[obj.id])
+        return format_html('<a class="btn btn-primary btn-sm" href="{}">Edit</a>', edit_url)
 
     def delete_button(self, obj):
-        # Generates a Delete link for the object
-        return format_html('<a class="btn btn-danger btn-sm" href="{}">Delete</a>', 
-                           f'/admin/app_name/booking/{obj.id}/delete/')
+        # Generate the URL for the delete page dynamically
+        delete_url = reverse('admin:Classes_booking_delete', args=[obj.id])
+        return format_html('<a class="btn btn-danger btn-sm" href="{}">Delete</a>', delete_url)
 
     edit_button.short_description = 'Edit'
     delete_button.short_description = 'Delete'
