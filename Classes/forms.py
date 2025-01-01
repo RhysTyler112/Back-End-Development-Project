552daa2886000ef1885b71b56b151a9557ca9ff4
Rhys Tyler <rhystyler.cardiff@gmail.com>
2025-01-01 13:28:22 +0000

    update python files per CI python linter

--- Classes/forms.py
+++ Classes/forms.py
@@ -1,7 +1,14 @@
 from django import forms
 from .models import Booking
 
+
 class BookingForm(forms.ModelForm):
     class Meta:
         model = Booking
-        fields = ['first_name', 'last_name', 'email','gym_class', 'experience_level', 'further_information']
+        fields = [
+            'first_name',
+            'last_name',
+            'email',
+            'gym_class',
+            'experience_level',
+            'further_information']
