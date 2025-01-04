3220397e29b8ef1b4bdec26a0ad276aac23642c5
Rhys Tyler <rhystyler.cardiff@gmail.com>
2025-01-04 15:54:24 +0000

    fix code to show error message and wont let form be submitted if options that are needed are not chosen

--- Classes/forms.py
+++ Classes/forms.py
@@ -12,3 +12,9 @@ class BookingForm(forms.ModelForm):
             'gym_class',
             'experience_level',
             'further_information']
+
+    def clean_gym_class(self):
+        gym_class = self.cleaned_data.get('gym_class')
+        if not gym_class:
+            raise forms.ValidationError("You must select a class before submitting the form.")
+        return gym_class
